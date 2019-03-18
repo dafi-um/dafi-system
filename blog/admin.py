7638ed648757e2b28ad33f3b8a266434efdaf54c
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-03-18 18:16:46 +0100

    Added markdown support to blog posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from django.db import models
+
+from pagedown.widgets import AdminPagedownWidget
 
 from .models import Post
+
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
     list_filter = ['pub_date']
+
+    formfield_overrides = {
+        models.TextField: {
+            'widget': AdminPagedownWidget
+        }
+    }
 
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField(max_length=5000)
-    pub_date = models.DateTimeField('date published')
+    '''
+    Blog Post
+    '''
+
+    title = models.CharField(_('title'), max_length=200)
+    content = models.TextField(_('content'), max_length=5000)
+    pub_date = models.DateTimeField(_('date published'))
 
     def __str__(self):
         return self.title
