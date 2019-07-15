d0c7c4f4e2f3c8a9be96411c6788f27118ddc2ed
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-07-15 13:13:42 +0200

    Minor changes

--- website/urls.py
+++ website/urls.py
@@ -4,12 +4,11 @@ from django.contrib import admin
 from django.contrib.flatpages import views
 from django.urls import include, path
 
+admin.site.site_header = 'Administración de DAFI'
+
 urlpatterns = [
     path('', include('main.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('p/<path:url>', views.flatpage),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
