4093fd8c702575da58e844b72115725502cc7432
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-07-19 12:57:27 +0200

    Added latest meetings section to homepage

--- main/views.py
+++ main/views.py
@@ -1,11 +1,15 @@
+from datetime import date
+
 from django.shortcuts import render
 
 from blog.models import Post
+from clubs.models import ClubMeeting
 
 
 def index(request):
     ctx = {
-        'last_post': Post.objects.all().order_by('-pub_date').first()
+        'last_post': Post.objects.all().order_by('-pub_date').first(),
+        'meetings': ClubMeeting.objects.all().exclude(moment__lt=date.today()).order_by('moment')[:5],
     }
 
     return render(request, 'main/index.html', context=ctx)
