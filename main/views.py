b0dfcc31241da0f813b79c04b1545f2afbcf4090
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-07-24 09:39:20 +0200

    Fixed runtime warning because of timezones

--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
-from datetime import date
-
 from django.shortcuts import render
+from django.utils import timezone
 
 from blog.models import Post
 from clubs.models import ClubMeeting
@@ -9,7 +8,7 @@ from clubs.models import ClubMeeting
 def index(request):
     ctx = {
         'last_post': Post.objects.all().order_by('-pub_date').first(),
-        'meetings': ClubMeeting.objects.all().exclude(moment__lt=date.today()).order_by('moment')[:5],
+        'meetings': ClubMeeting.objects.all().exclude(moment__lt=timezone.now()).order_by('moment')[:5],
     }
 
     return render(request, 'main/index.html', context=ctx)
