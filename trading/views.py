cb78a0f55085ef5abd0794bf0d700307eafa8864
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-08-01 18:39:17 +0200

    Fixed trading views imports

--- trading/views.py
+++ trading/views.py
@@ -1,10 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.http import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, ListView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-from .forms import TradeOfferForm
 from .models import TradeOffer
 
 
