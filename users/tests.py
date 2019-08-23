cfd02f3cae816e901232583822ad805639271280
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-08-23 12:55:39 +0200

    Added tests for users templatetags

---
+++ users/tests.py
@@ -0,0 +1,34 @@
+from django.template import Context, Template
+from django.test import TestCase
+
+class TradeOfferTestCase(TestCase):
+    def render_nice_name(self, user):
+        template_str = '{% load users_tags %}{{ user|nice_name }}'
+        context = Context({'user': user})
+        return Template(template_str).render(context)
+
+    def test_nice_name_returns_username(self):
+        '''nice_name tag returns the username when the full name is not available'''
+
+        class UserNoName():
+            username = 'my_username'
+
+            def get_full_name(self):
+                return None
+
+        rendered = self.render_nice_name(UserNoName())
+
+        self.assertEquals(rendered, 'my_username')
+
+    def test_nice_name_returns_full_namename(self):
+        '''nice_name tag returns the full name when is available'''
+
+        class User():
+            username = 'my_username'
+
+            def get_full_name(self):
+                return 'my_full_name'
+
+        rendered = self.render_nice_name(User())
+
+        self.assertEquals(rendered, 'my_full_name')
