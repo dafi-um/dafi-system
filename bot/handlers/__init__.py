1da92555e572e5cf4ca53912f241d896b58e9b99
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-09-15 22:20:43 +0200

    Updated blog bot handler

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,3 +3,4 @@ from .handlers import get_handlers
 from .basic import *
 from .groups import *
 from .users import *
+from .blog import *
--- bot/handlers/blog.py
+++ bot/handlers/blog.py
@@ -1,56 +1,25 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 from blog.models import Post
 
-def generate_blog_list():
-    msg = '*El Blog de DAFI*\n\n'
-    reply_markup = None
+from main.utils import get_url
 
-    posts = Post.objects.all()
+from .handlers import add_handler
+
+@add_handler('blog')
+def blog_list(update, context):
+    posts = Post.objects.order_by('-pub_date')[:10]
 
     if not posts:
-        msg += '_No hay entradas disponibles_'
-    else:
-        buttons = []
+        return '*El Blog de DAFI*\n\n_No hay entradas disponibles_'
 
-        for p in posts:
-            txt = '{} ({})'.format(p.title, p.pub_date.strftime('%d %b %y'))
-            buttons.append([InlineKeyboardButton(txt, callback_data='blog=' + p.slug)])
+    msg = '*El Blog de DAFI*\n\nMostrando los últimos {} posts.'.format(len(posts))
 
-        reply_markup = InlineKeyboardMarkup(buttons)
+    buttons = []
 
-    return msg, reply_markup
+    for p in posts:
+        buttons.append([
+            InlineKeyboardButton(p.title, url=get_url('blog:detail', args=[p.slug]))
+        ])
 
-def blog_list(update, context):
-    msg, reply_markup = generate_blog_list()
-
-    update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
-
-def blog_detail(update, context):
-    post = None
-    slug = update.callback_query.data.replace('blog=', '')
-
-    msg = '_No se ha encontrado el artículo que buscabas_'
-    reply_markup = None
-
-    if slug == '.List':
-        msg, reply_markup = generate_blog_list()
-    elif slug:
-        try:
-            post = Post.objects.get(slug=slug)
-        except:
-            pass
-
-        if post:
-            msg = '*- ' + post.title + ' -*\n\n' + post.content
-
-        buttons = [[InlineKeyboardButton('<< Volver a la lista', callback_data='blog=.List')]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-
-    update.callback_query.answer()
-    update.callback_query.edit_message_text(msg, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
-
-def add_handlers(dispatcher):
-    dispatcher.add_handler(CommandHandler('blog', blog_list))
-    dispatcher.add_handler(CallbackQueryHandler(blog_detail, pattern='blog'))
+    return msg, InlineKeyboardMarkup(buttons)
