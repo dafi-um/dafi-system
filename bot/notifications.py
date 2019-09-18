faa75c5821c17a61915e69adab51c81116bf2443
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-09-18 12:06:16 +0200

    Fixed minor type conversion error

--- bot/notifications.py
+++ bot/notifications.py
@@ -17,7 +17,7 @@ def telegram_notify(user, message, url=None, url_button=None):
 
     if url and url_button:
         reply_markup = InlineKeyboardMarkup([[
-            InlineKeyboardButton(url_button, url=get_domain() + url),
+            InlineKeyboardButton(url_button, url=get_domain() + str(url)),
         ]])
 
     _bot.send_message(user.telegram_id, message, reply_markup=reply_markup)
