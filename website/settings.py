220ae06a3d328b7eb6a50f801e8b6a06c48f3f42
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-09-27 20:03:59 +0200

    Enabled static files caching

--- website/settings.py
+++ website/settings.py
@@ -167,6 +167,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
