a2f963909f5497e4c3fa1736f581c62a07c51e07
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-10-01 18:13:29 +0200

    Bot rooms handler now uses persistence

--- bot/handlers/rooms.py
+++ bot/handlers/rooms.py
@@ -4,7 +4,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 from django.contrib.auth import get_user_model
 
-from heart.models import Room
+from .. import persistence
 
 from .handlers import add_handler, CommandHandler, QueryHandler
 
@@ -13,18 +13,83 @@ User = get_user_model()
 DAFI_ROOM_CODE = getenv('DAFI_ROOM_CODE', 'dafi')
 DAFI_MAIN_GROUP = getenv('DAFI_MAIN_GROUP', None)
 
+ROOM_MEMBERS_LIST = 'room_members'
+ROOM_QUEUE_LIST = 'room_queue'
+
+
+class RoomMixin():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._members = None
+        self._refresh_members()
+
+        self._queue = None
+        self._refresh_queue()
+
+    def _refresh_members(self):
+        self._members = persistence.get_item(ROOM_MEMBERS_LIST, [])
+
+    def _sync_members(self):
+        persistence.set_item(ROOM_MEMBERS_LIST, self._members)
+
+    def enter_room(self, user):
+        self._refresh_members()
+        self._members.append(user)
+        self._sync_members()
+
+    def leave_room(self, user):
+        self._refresh_members()
+        self._members.remove(user)
+        self._sync_members()
+
+    def room_empty(self):
+        self._refresh_members()
+        return len(self._members) == 0
+
+    def in_room(self, user):
+        self._refresh_members()
+        return user in self._members
+
+    def _refresh_queue(self):
+        self._queue = persistence.get_item(ROOM_QUEUE_LIST, [])
+
+    def _sync_queue(self):
+        persistence.set_item(ROOM_QUEUE_LIST, self._queue)
+
+    def in_queue(self, user_id):
+        self._refresh_queue()
+        return user_id in self._queue
+
+    def add_to_queue(self, user_id):
+        self._refresh_queue()
+        self._queue.append(user_id)
+        self._sync_queue()
+
+    def get_queue(self):
+        self._refresh_queue()
+        return self._queue
+
+    def clear_queue(self):
+        self._queue = []
+        self._sync_queue()
+
 
 @add_handler('dafi')
-class DafiRoom(CommandHandler):
+class DafiRoom(RoomMixin, CommandHandler):
     def handle(self, update, context):
-        room = Room.objects.filter(code=DAFI_ROOM_CODE).first()
-
-        if not room:
-            return '⚠️⚠️\n¡¡La sala no existe en la base de datos!!'
-
         if not context.args:
-            if not room.members.all():
-                return 'Ahora mismo no hay nadie en DAFI 😓'
+            if self.room_empty():
+                reply_markup = InlineKeyboardMarkup([[
+                    InlineKeyboardButton(
+                        'Avísame cuando llegue alguien ✔️',
+                        callback_data='dafi:notify:{}'.format(update.effective_user.id)
+                    )
+                ], [
+                    InlineKeyboardButton('No me avises ❌', callback_data='main:okey')
+                ]])
+
+                return 'Ahora mismo no hay nadie en DAFI 😓', reply_markup
 
             if update.message.chat.type != 'private':
                 return 'Hay alguien en DAFI ✅'
@@ -32,7 +97,7 @@ class DafiRoom(CommandHandler):
             msg = 'Hay alguien en DAFI, ¿quieres que avise de que vas?'
             reply_markup = InlineKeyboardMarkup([
                 [InlineKeyboardButton('Sí, estoy de camino 🏃🏻‍♂️', callback_data='dafi:omw')],
-                [InlineKeyboardButton('No, iré luego ☕️', callback_data='dafi:later')]
+                [InlineKeyboardButton('No, iré luego ☕️', callback_data='main:okey')]
             ])
 
             return msg, reply_markup
@@ -47,13 +112,18 @@ class DafiRoom(CommandHandler):
         if not user or not user.has_perm('heart.can_change_room_state'):
             return 'No puedes llevar a cabo esta acción'
 
-        room = Room.objects.get()
-
         if action == 'on':
-            if user in room.members.all():
+            if self.in_room(user):
                 return 'Ya tenía constancia de que estás en DAFI ⚠️'
 
-            room.members.add(user)
+            self.enter_room(user)
+
+            msg = '@{} acaba de llegar a DAFI 🔔'.format(user.telegram_user)
+
+            for user_id in self.get_queue():
+                context.bot.send_message(user_id, msg)
+
+            self.clear_queue()
 
             reply_markup = InlineKeyboardMarkup([[
                 InlineKeyboardButton('Me voy 💤', callback_data='dafi:off')
@@ -62,29 +132,23 @@ class DafiRoom(CommandHandler):
             return 'He anotado que estás DAFI ✅', reply_markup
 
         else:
-            if user not in room.members.all():
+            if not self.in_room(user):
                 return 'No sabía que estabas en DAFI ⚠️'
 
-            room.members.remove(user)
+            self.leave_room(user)
+
             return 'He anotado que has salido de DAFI ✅'
 
 
 @add_handler('dafi')
-class DafiCallback(QueryHandler):
+class DafiCallback(RoomMixin, QueryHandler):
     def handle(self, update, context):
         query = update.callback_query
-        action = query.data.replace('dafi:', '')
-
-        if action == 'later':
-            return '¡De acuerdo!'
-
-        room = Room.objects.filter(code=DAFI_ROOM_CODE).first()
-
-        if not room:
-            return '⚠️⚠️\n¡¡La sala no existe en la base de datos!!'
+        parts = query.data.replace('dafi:', '').split(':')
+        action = parts[0]
 
         if action == 'omw':
-            if not room.members.all():
+            if self.room_empty():
                 return 'Ahora mismo no hay nadie en DAFI 😓'
 
             if DAFI_MAIN_GROUP:
@@ -92,15 +156,19 @@ class DafiCallback(QueryHandler):
                 context.bot.sendMessage(DAFI_MAIN_GROUP, text=text)
 
             return 'Hecho, les he avisado 😉'
+        elif action == 'notify':
+            self.add_to_queue(update.effective_user.id)
+
+            return 'Hecho, te avisaré 😉'
         elif action == 'off':
             user = self.get_user()
 
             if not user:
                 return 'No he encontrado una cuenta para tu usuario ⚠️'
 
-            if user not in room.members.all():
+            if not self.in_room(user):
                 return 'No sabía que estabas en DAFI ⚠️'
 
-            room.members.remove(user)
+            self.leave_room(user)
 
             return 'He anotado que has salido de DAFI  ✅'
