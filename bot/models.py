819c1b99f8b3613eabe1ef565b67560de6dce9eb
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-10-07 19:47:04 +0200

    Updated bot permissions

--- bot/models.py
+++ bot/models.py
@@ -9,4 +9,5 @@ class BotPermissions(models.Model):
         default_permissions = ()
         permissions = (
             ('can_change_room_state', 'Puede cambiar el estado de una sala'),
+            ('can_manage_elections', 'Puede gestionar operaciones del periodo de elecciones'),
         )
