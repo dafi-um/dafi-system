f34c60b47ae3ceecd58887682e75d073c9ecbaa9
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-10-11 18:20:48 +0200

    Added metatags to blog views

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,43 @@
 from django.contrib.auth import get_user_model
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.db import models
+from django.utils.text import Truncator
+
+from meta.models import ModelMeta
 
 
-class Post(models.Model):
+class Post(ModelMeta, models.Model):
     '''
     Blog Post
     '''
 
     title = models.CharField('título', max_length=200)
+
     slug = models.SlugField('slug', max_length=200)
+
     content = models.TextField('contenido', max_length=5000)
+
     pub_date = models.DateTimeField('fecha de publicación')
-    author = models.ForeignKey(get_user_model(),
-                               on_delete=models.PROTECT,
-                               verbose_name='autor')
-    image = models.ImageField(upload_to='blog/', verbose_name='imagen', blank=True)
+
+    author = models.ForeignKey(
+        get_user_model(), models.PROTECT, verbose_name='autor'
+    )
+
+    image = models.ImageField(
+        upload_to='blog/', verbose_name='imagen', blank=True
+    )
+
+    _metadata = {
+        'title': 'title',
+        'description': 'get_abstract',
+        'image': 'get_image'
+    }
 
     def __str__(self):
         return self.title
+
+    def get_abstract(self):
+        return Truncator(self.content).chars(200)
+
+    def get_image(self):
+        return self.image.url if self.image else static('images/favicon.png')
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import DetailView, ListView
+
+from meta.views import MetadataMixin
 
 from .models import Post
 
 
-class IndexView(generic.ListView):
+class IndexView(MetadataMixin, ListView):
+    title = 'El blog de DAFI'
+    description = 'Noticias de la Delegación de Estudiantes de Informática'
+    image = 'images/favicon.png'
+
     def get_queryset(self):
         """Return the last five published posts."""
         return Post.objects.order_by('-pub_date')[:5]
 
 
-class DetailView(generic.DetailView):
+class DetailView(MetadataMixin, DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['meta'] = self.get_object().as_meta(self.request)
+        return context
