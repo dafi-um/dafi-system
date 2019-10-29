ce91e656f07afd75b3b911243e582304147e3814
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2019-10-29 15:35:36 +0100

    Created bot utilities module

---
+++ bot/utils.py
@@ -0,0 +1,31 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+def create_reply_markup(*lines):
+    buttons = []
+
+    for items in lines:
+        line = []
+
+        for text, data, *extra in items:
+            url = extra[0] if extra else None
+
+            line.append(
+                InlineKeyboardButton(text, callback_data=data, url=url)
+            )
+
+        buttons.append(line)
+
+    return InlineKeyboardMarkup(buttons)
+
+def create_users_list(users):
+    if not users:
+        return '\nNo hay usuarios para mostrar...'
+
+    msg = ''
+
+    for user in users:
+        msg += '\n[{}](tg://user?id={})'.format(
+            user.get_full_name(), user.telegram_id
+        )
+
+    return msg
