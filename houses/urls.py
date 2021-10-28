42307fa342ee01423cf109d8585ee7ae3f55cc09
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2021-10-28 17:54:57 +0200

    Creada vista de perfil de casas, creado placeholder para algoritmo seleccionador

--- houses/urls.py
+++ houses/urls.py
@@ -7,4 +7,6 @@ app_name = 'houses'
 
 urlpatterns = [
     path('', views.HousesListView.as_view(), name='list'),
+    path('perfil/<int:pk>/', views.HouseProfileDetailView.as_view(), name='profile'),
+    path('algoritmo-seleccionador/', views.SelectorAlgorithmView.as_view(), name='selector'),
 ]
--- houses/views.py
+++ houses/views.py
@@ -1,12 +1,21 @@
+from typing import Any
+
+from django.contrib.auth.mixins import AccessMixin
 from django.db.models import QuerySet
+from django.http import HttpRequest
+from django.http.response import HttpResponseBase
+from django.shortcuts import redirect
 from django.views.generic import (
+    DetailView,
     ListView,
+    TemplateView,
 )
 
 from meta.views import MetadataMixin
 
 from .models import (
     House,
+    HouseProfile,
 )
 
 
@@ -41,4 +50,49 @@ class HousesListView(MetadataMixin, ListView):
         if not any(house != best_house and house.points == best_house.points for house in houses):
             context['best_house'] = best_house
 
+        if self.request.user.is_authenticated:
+            context['profile'] = HouseProfile.objects.filter(user=self.request.user).first()
+
         return context
+
+
+class HouseProfileDetailView(MetadataMixin, DetailView):
+
+    model = HouseProfile
+
+    context_object_name = 'profile'
+
+    description = META_DESCRIPTION
+    image = META_IMAGE
+
+    def get_queryset(self) -> 'QuerySet[HouseProfile]':
+        return super().get_queryset().prefetch_related('user', 'house')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context['my_profile'] = context['profile'].user == self.request.user
+
+        return context
+
+    def get_meta_title(self, context: dict[str, Any]):
+        return f'Perfil de {context["profile"].display_name} - Las Casas de la FIUM'
+
+
+class SelectorAlgorithmView(AccessMixin, MetadataMixin, TemplateView):
+
+    template_name = 'houses/selector_algorithm.html'
+
+    title = 'Algoritmo Seleccionador - Las Casas de la FIUM'
+    description = META_DESCRIPTION
+    image = META_IMAGE
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponseBase:
+        if not self.request.user.is_authenticated:
+            return self.handle_no_permission()
+
+        if HouseProfile.objects.filter(user=self.request.user).exists():
+            return redirect('houses:profile')
+
+        return super().dispatch(request, *args, **kwargs)
