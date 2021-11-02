16bd543f9aa6dbc29c4e3b4d7f22b02483c360b1
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2021-11-02 14:57:08 +0100

    Mejoras menores

--- sanalberto/admin.py
+++ sanalberto/admin.py
@@ -22,25 +22,32 @@ class EventAdmin(admin.ModelAdmin):
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'event', 'start', 'end', 'club', 'is_public')
+    list_display = ('title', 'event', 'start', 'end', 'public')
     list_filter = ('event', 'is_public')
     list_select_related = ('event', 'club')
 
-    search_fields = ('title', 'club', 'event')
+    search_fields = ('title', 'id', 'club__name', 'club__id')
 
     autocomplete_fields = ('event', 'club', 'organisers', 'documents')
+
+    # To change the column name
+    @admin.display(boolean=True, ordering='is_public', description='Público')
+    def public(self, obj: Activity) -> bool:
+        return obj.is_public
 
 
 @admin.register(ActivityRegistration)
 class ActivityRegistrationAdmin(admin.ModelAdmin):
 
     list_display = ('activity', 'user', 'is_paid')
-    list_filter = ('is_paid',)
+    list_filter = ('activity__event', 'is_paid')
     list_select_related = ('activity', 'user')
 
-    search_fields = ('id', 'activity__id', 'activity__title', 'user__id', 'user__email')
+    search_fields = ('id', 'activity__title', 'user__email')
 
     autocomplete_fields = ('activity', 'user')
+
+    readonly_fields = ('created', 'updated')
 
 
 @admin.register(Poll)
@@ -50,7 +57,7 @@ class PollAdmin(admin.ModelAdmin):
     list_filter = ('event',)
     list_select_related = ('event',)
 
-    search_fields = ('title', 'slug', 'event')
+    search_fields = ('id', 'title', 'slug')
 
     autocomplete_fields = ('winner',)
 
@@ -60,14 +67,15 @@ class PollAdmin(admin.ModelAdmin):
 @admin.register(PollDesign)
 class PollDesignAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'approved', 'user', 'poll')
+    list_display = ('title', 'approved', 'user', 'poll', 'created')
     list_select_related = ('poll', 'user')
+    list_filter = ('poll__event', 'is_approved')
 
-    search_fields = ('title', 'user', 'poll')
+    search_fields = ('id', 'title', 'user__email', 'poll__title')
 
     readonly_fields = ('created',)
 
-    # To change the column name from 'Diseño aprobado' to 'Aprobado' (:
+    # To change the column name
     @admin.display(boolean=True, ordering='is_approved', description='Aprobado')
     def approved(self, obj: PollDesign) -> bool:
         return obj.is_approved
@@ -76,8 +84,11 @@ class PollDesignAdmin(admin.ModelAdmin):
 @admin.register(PollVote)
 class PollVoteAdmin(admin.ModelAdmin):
 
-    list_display = ('__str__', 'poll', 'user')
+    list_display = ('__str__', 'poll', 'user', 'updated')
     list_select_related = ('poll', 'user')
+    list_filter = ('poll__event', 'created', 'updated')
+
+    search_fields = ('id', 'user__email', 'poll__title')
 
     autocomplete_fields = ('poll', 'user', 'first', 'second', 'third')
 
